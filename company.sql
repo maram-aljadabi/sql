7d6af4f836f7997aa7c69fb718faaca8a5776597
maram-aljadabi <maramaljadabi@gmail.com>
2025-07-24 21:24:32 +0300

    inner join

--- company.sql
+++ company.sql
@@ -1,13 +1,4 @@
 use company;
-create table company(
-    company_id int primary key ,
-    company_name varchar(225),
-    company_city varchar(110)
-);
-create table product(
-    product_id int not null,
-    product_name varchar(225),
-    company_id int,
-primary key (product_id),
-foreign key (company_id) references company(company_id));
-select * from product;
+select product_id,company_name from product
+inner join company
+on product.company_id=company.company_id;
