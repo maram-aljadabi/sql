1d88b41d943417888f610ef017194fbb6a1c57e9
maram-aljadabi <maramaljadabi@gmail.com>
2025-07-24 21:36:02 +0300

    left & right outer join

--- company.sql
+++ company.sql
@@ -1,4 +1,13 @@
 use company;
-select product_id,company_name from product
-inner join company
-on product.company_id=company.company_id;
+create table project(
+    project_id int primary key ,
+    project_manager varchar(225),
+    project_start date
+);
+select id,emp_name,project_id from employee
+left outer join project
+on emp_name=project_manager;
+
+select id,emp_name,project_id,project_manager from employee
+right outer join project
+on emp_name=project_manager;
