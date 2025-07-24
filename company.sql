8a287cd4ad6891c6eb268c4e4f5594a55069aac4
maram-aljadabi <maramaljadabi@gmail.com>
2025-07-24 21:19:51 +0300

    add foreign key

--- company.sql
+++ company.sql
@@ -1,2 +1,13 @@
-
-
+use company;
+create table company(
+    company_id int primary key ,
+    company_name varchar(225),
+    company_city varchar(110)
+);
+create table product(
+    product_id int not null,
+    product_name varchar(225),
+    company_id int,
+primary key (product_id),
+foreign key (company_id) references company(company_id));
+select * from product;
