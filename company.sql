17fbc40463698644e096f76ef4951c13095483cd
maram-aljadabi <maramaljadabi@gmail.com>
2025-07-24 21:43:03 +0300

    cross join

--- company.sql
+++ company.sql
@@ -1,13 +1,2 @@
 use company;
-create table project(
-    project_id int primary key ,
-    project_manager varchar(225),
-    project_start date
-);
-select id,emp_name,project_id from employee
-left outer join project
-on emp_name=project_manager;
-
-select id,emp_name,project_id,project_manager from employee
-right outer join project
-on emp_name=project_manager;
+select * from employee cross join company;
